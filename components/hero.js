26a163ba13c8fc63ed7a611007a0c0aa66106764
Irina Illustrova <illustrova@gmail.com>
2021-03-02 23:49:45 +0200

    wrap hero tet with container

--- components/hero.js
+++ components/hero.js
@@ -1,7 +1,7 @@
 export const HERO_HEIGHT = 64; //4rem
 export default function Hero({ children }) {
   return (
-    <div className="py-16 container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+    <div className="py-16 container max-w-5xl px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       {children}
     </div>
   );
