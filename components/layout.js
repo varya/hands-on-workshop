49a1c43434e8c9b92ab4f8a17eb57552bb694322
Irina Illustrova <illustrova@gmail.com>
2021-03-29 02:53:21 +0300

    remove header from handout page

--- components/layout.js
+++ components/layout.js
@@ -1,12 +1,9 @@
-import Header from "./header";
 import Footer from "./footer";
 
 export default function Layout(props) {
   return (
     <div className="flex flex-col min-h-screen">
-      <Header />
-
-      {/* <main className="flex-1 lg:container px-4 py-6 mx-auto md:px-6 md:py-12"> */}
+      {/* <main className="flex-1 px-4 py-6 mx-auto lg:container md:px-6 md:py-12"> */}
       {props.children}
       {/* </main> */}
 
