9442e8756231d7bf3662fc803e78452be71742a5
Irina Illustrova <illustrova@gmail.com>
2021-03-29 15:27:09 +0300

    quick fix for dst

--- components/local-time.js
+++ components/local-time.js
@@ -1,3 +1,12 @@
+const stdTimezoneOffset = function () {
+  var jan = new Date(this.getFullYear(), 0, 1);
+  var jul = new Date(this.getFullYear(), 6, 1);
+  return Math.max(jan.getTimezoneOffset(), jul.getTimezoneOffset());
+};
+
+const isDstObserved = () =>
+  new Date().getTimezoneOffset() < stdTimezoneOffset();
+
 function getTimezoneAbbr(tzOffset) {
   switch (tzOffset) {
     case 0:
@@ -11,15 +20,20 @@ function getTimezoneAbbr(tzOffset) {
       return "EET";
   }
 }
-export default function LocalTime({ start, end, format = "%start%-%end% %timezone%" }) {
-  const tzOffset = -new Date().getTimezoneOffset() / 60;
+export default function LocalTime({
+  start,
+  end,
+  format = "%start%-%end% %timezone%",
+}) {
+  let tzOffset = -new Date().getTimezoneOffset() / 60;
+  tzOffset = isDstObserved ? tzOffset - 1 : tzOffset;
   if (tzOffset >= 0 && tzOffset <= 3) {
     start = start - 2 + tzOffset;
     end = end - 2 + tzOffset;
   }
   const zoneAbbr = getTimezoneAbbr(tzOffset);
   return format
-          .replace('%start%', start)
-          .replace('%end%', end)
-          .replace('%timezone%', zoneAbbr);
+    .replace("%start%", start)
+    .replace("%end%", end)
+    .replace("%timezone%", zoneAbbr);
 }
