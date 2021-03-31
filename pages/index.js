1330ac0309dd8ee96dcdf0d7a5ccfd888dd8feee
Bridge Tools <robot@bridgetools.io>
2021-03-31 11:45:32 +0300

    Adjust the number of tickets

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,7 @@ export default function IndexPage() {
             Register
           </Button>
           <p className="m-6 mx-6 text-sm text-bold">
-            <strong>Available tickets:</strong> 12 for designers, 12 for
+            <strong>Available tickets:</strong> 10 for designers, 10 for
             developers
           </p>
         </div>
