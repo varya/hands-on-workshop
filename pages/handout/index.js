7f327c11b598757c1bd703f08d721cec60ef1aef
Bridge Tools <robot@bridgetools.io>
2021-03-31 11:44:20 +0300

    Structure for Handout pages

--- pages/handout/index.js
+++ pages/handout/index.js
@@ -22,36 +22,64 @@ const blocks = [
     roles: ["designer", "developer"],
   },
   {
+    title: "UI Inventory",
+    image: BoxIcon,
+    slug: "./inventory",
+    roles: [],
+  },
+  {
     title: "Setting up the project",
     image: ComponentsIcon,
     slug: "setup",
     roles: ["developer"],
   },
-  // { title: "Naming", image: ComponentsIcon, slug: "./naming" },
-  { title: "UI Inventory", image: BoxIcon, slug: "./inventory", roles: [] },
   {
-    title: "Parts of design system",
-    image: LegoIcon,
-    slug: "parts",
-    roles: ["designer", "developer"],
-  },
-  { title: "Pattern Journey", image: MapIcon, slug: "./journey", roles: [] },
-  {
-    title: "Visual regression testing",
-    image: TestingIcon,
-    slug: "test",
+    title: "Library release",
+    image: DeploymentIcon,
+    slug: "release",
     roles: ["developer"],
   },
   {
-    title: "Build and Deployment",
-    image: DeploymentIcon,
-    slug: "release",
+    title: "Product update",
+    image: DeploymentIcon, // TODO: change icon
+    slug: "product-update",
+    roles: ["developer"],
+  },
+  {
+    title: "Re-design",
+    image: DeploymentIcon, // TODO: change icon
+    slug: "re-design",
+    roles: ["designer"],
+  },
+  {
+    title: "Product release",
+    image: DeploymentIcon, // TODO: change icon
+    slug: "product-release",
+    roles: ["developer"],
+  },
+  {
+    title: "Design changes",
+    image: LegoIcon, // TODO: change icon
+    slug: "design-chages",
+    roles: ["designer", "developer"],
+  },
+  {
+    title: "Visual regression tests",
+    image: TestingIcon, // TODO: change icon
+    slug: "visual-tests",
+    roles: ["developer"],
+  },
+  {
+    title: "Automatic syncronizations",
+    image: ComponentsIcon, // TODO: change icon
+    slug: "autosync",
     roles: ["developer"],
   },
   {
     title: "Feedback Form",
     image: FeedbackIcon,
     slug: "feedback",
+    url: "https://varyastepanova.typeform.com/to/uE8xtz4B",
     roles: [],
   },
 ];
@@ -68,12 +96,13 @@ export default function HandoutPage() {
       <div className="flex-1 w-full h-full bg-gray-200">
         <div className="container flex flex-wrap max-w-5xl m-8 mx-auto">
           {blocks.map((block) => {
+            const link = block.url ? block.url : `${pathPrefix}/${block.slug}`;
             return (
               <Card key={block.slug} className="sm:w-1/2 md:w-1/3 group">
                 <div className="w-full h-full px-16 pt-4 pb-8 md:px-8">
                   {<block.image className="w-full h-full p-2" />}
                 </div>
-                <Link href={`${pathPrefix}/${block.slug}`}>
+                <Link href={link}>
                   <a className="absolute top-0 left-0 w-full h-full cursor-pointer ">
                     <div className="flex items-center justify-center w-full h-full text-lg text-center text-white uppercase transition duration-300 bg-gray-700 opacity-90 group-hover:bg-transparent text-bold group-hover:transform group-hover:-translate-y-1/1 group-hover:text-transparent">
                       <div>{block.title}</div>
