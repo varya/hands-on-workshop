5044c09701beaf535c1eb8a5d2be662122e90342
Bridge Tools <robot@bridgetools.io>
2021-05-21 16:57:24 +0300

    Workshop material @v2

---
+++ pages/handout/v2/feedback.js
@@ -0,0 +1,12 @@
+export default function Feedback() {
+  return (
+    <div className="flex-1 w-full h-full pt-8 bg-white">
+      <div className="container flex-1 h-full max-w-5xl py-6 mx-auto bg-white">
+        <h2 className="text-3xl font-bold text-center text-gray-800">
+          Heading
+        </h2>
+        <p className="pt-8 text-lg text-gray-800">Text goes here</p>
+      </div>
+    </div>
+  );
+}
