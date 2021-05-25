65d0eff57f5b1991b44d0a6800354eec631a0607
Bridge Tools <robot@bridgetools.io>
2021-05-25 12:15:05 +0300

    Option to disabled second day tasks

--- pages/handout/index.js
+++ pages/handout/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import clsx from "clsx";
 
 import Hero from "@components/hero";
 import Card from "@components/card";
@@ -27,72 +28,84 @@ const blocks = [
     image: Flow,
     slug: "workshop-flow",
     roles: [],
+    day: 1,
   },
   {
     title: "Making a toast",
     image: ToastIcon,
     slug: "toast",
     roles: ["designer", "developer"],
+    day: 1,
   },
   {
     title: "Figma setup",
     image: SettingsIcon,
     slug: "figma-setup",
     roles: ["designer"],
+    day: 1,
   },
   {
     title: "Repository setup",
     image: SetupIcon,
     slug: "repo-setup",
     roles: ["developer"],
+    day: 1,
   },
   {
     title: "UI inventory",
     image: BoxIcon,
     slug: "ui-inventory",
     roles: ["developer", "designer"],
+    day: 1,
   },
   {
     title: "Design tokens",
     image: Sketch,
     slug: "design-tokens",
     roles: ["designer"],
+    day: 1,
   },
   {
     title: "Coding the library",
     image: CodingIcon,
     slug: "coding-library",
     roles: ["developer"],
+    day: 1,
   },
   {
     title: "Releasing the library",
     image: FlagIcon,
     slug: "releasing-library",
     roles: ["developer"],
+    day: 2,
   },
   {
     title: "Product update",
     image: ComponentsIcon,
     slug: "product-update",
     roles: ["developer"],
+    day: 2,
   },
   {
     title: "Design Changes",
     image: LegoIcon,
     slug: "re-design",
     roles: ["designer"],
+    day: 2,
   },
   {
     title: "Cast in code",
     image: CheckList,
     slug: "cast-in-code",
     roles: ["developer"],
+    day: 2,
   },
   {
     title: "Product release",
     image: DeploymentIcon,
     slug: "product-release",
     roles: ["developer"],
+    day: 2,
   },
   // {
   //   title: "Visual regression tests",
@@ -105,6 +118,7 @@ const blocks = [
     image: SyncIcon,
     slug: "autosync",
     roles: ["developer"],
+    day: 2,
   },
   {
     title: "Feedback Form",
@@ -113,8 +127,37 @@ const blocks = [
     url:
       "https://docs.google.com/forms/d/124ha3LG7-07rizB9yjoqNGkB31Yzw6jtFvImsz7-Utg",
     roles: ["designer", "developer"],
+    day: 2,
   },
 ];
+
+function Blocks(props) {
+  return (
+    <>
+      {blocks.map((block) => {
+            const link = block.url ? block.url : `${pathPrefix}/${block.slug}`;
+            const enabled = block.day === props.day || props.day === "all";
+            return (
+              <Card key={block.slug} className={clsx("sm:w-1/2 md:w-1/3 group", enabled || "pointer-events-none opacity-50")}>
+                <div className="w-full h-full px-16 pt-4 pb-8 md:px-8">
+                  {<block.image className="w-full h-full p-2" />}
+                </div>
+                <Link href={link} disabled="yes">
+                  <a className="absolute top-0 left-0 w-full h-full cursor-pointer">
+                    <div className="flex items-center justify-center w-full h-full text-lg text-center text-white uppercase transition duration-300 bg-gray-700 opacity-90 group-hover:bg-transparent text-bold group-hover:transform group-hover:-translate-y-1/1 group-hover:text-transparent">
+                      <div>{block.title}</div>
+                    </div>
+                  </a>
+                </Link>
+                {block.roles.map((r) => (
+                  <Ribbon key={r} role={r} />
+                ))}
+              </Card>
+            );
+          })}
+    </>
+  )
+}
 
 export default function HandoutPage() {
   return (
@@ -127,26 +170,8 @@ export default function HandoutPage() {
       {/* CARDS SECTION */}
       <div className="flex-1 w-full h-full bg-gray-200">
         <div className="container flex flex-wrap max-w-5xl m-8 mx-auto">
-          {blocks.map((block) => {
-            const link = block.url ? block.url : `${pathPrefix}/${block.slug}`;
-            return (
-              <Card key={block.slug} className="sm:w-1/2 md:w-1/3 group">
-                <div className="w-full h-full px-16 pt-4 pb-8 md:px-8">
-                  {<block.image className="w-full h-full p-2" />}
-                </div>
-                <Link href={link}>
-                  <a className="absolute top-0 left-0 w-full h-full cursor-pointer ">
-                    <div className="flex items-center justify-center w-full h-full text-lg text-center text-white uppercase transition duration-300 bg-gray-700 opacity-90 group-hover:bg-transparent text-bold group-hover:transform group-hover:-translate-y-1/1 group-hover:text-transparent">
-                      <div>{block.title}</div>
-                    </div>
-                  </a>
-                </Link>
-                {block.roles.map((r) => (
-                  <Ribbon key={r} role={r} />
-                ))}
-              </Card>
-            );
-          })}
+          {/* <Blocks day={1}/> */}
+          <Blocks day={"all"}/>
         </div>
       </div>
     </>
